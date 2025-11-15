78d65329e6555b1905d9cba8e53e2f89f033afbe
Jatin Kushwaha <jatinkushwaha995@gmail.com>
2025-11-15 22:11:49 +0530

    new update

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-    await mongoose.connect(process.env.MONGODB_URL_LOCAL).then(() => {
+    await mongoose.connect(process.env.MONGODB_URL).then(() => {
         console.log('Database connected');
     }).catch((error)=>{
         console.log(error);
